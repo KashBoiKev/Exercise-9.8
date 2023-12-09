867c21368930c671772c6319d9e2f252cacbc2a5
KashBoiKev <67813689+KashBoiKev@users.noreply.github.com>
2023-12-09 17:44:52 -0500

    Add files via upload

---
+++ Excercise_9_8.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.excercise_9_8;
+
+/**
+ *
+ * @author user
+ */
+public class Excercise_9_8 {
+    
+    public static void main(String[] args) {
+        
+    // two fans objects are created with the following values 
+    
+    //Assign maximum speed, radius 10, color yellow, and turn it on to the first object.
+        Fan fan1 = new Fan();
+        fan1.setSpeed(3);
+        fan1.setOn(true);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        System.out.println(fan1.toString());
+        
+        
+    //Assign medium speed, radius 5, color blue, and turn it off to the second object.
+        Fan fan2 = new Fan();
+        fan2.setSpeed(2);
+        fan2.setOn(false);
+        fan2.setRadius(5);
+        fan2.setColor("blue");
+        System.out.println(fan2.toString());     
+
+        
+    }
+}
---
+++ Fan.java
@@ -0,0 +1,85 @@
+package com.mycompany.excercise_9_8;
+
+/**
+ *
+ * @author user
+ */
+public class Fan {
+    // Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+    
+    // Private int, boolean, double, and String data fields
+    private int speed;
+    private boolean on;
+    private double radius;
+    String color;
+
+    //Constructor with default fan values
+    public Fan() {
+        speed = SLOW;
+        on = false;
+        radius = 5;
+        color = "blue";
+    }
+    
+
+
+// Accessors and mutators 
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+   // Method named toString(); that returns a string description of the fan 
+    public String toString() {
+         // When fan is on it display the speed , color, and radius
+        if (on == true){
+            return "Fan 1: " + "Speed: " + (speed == 1 ? "SLOW" : speed == 2 ? "MEDIUM" : "FAST") +
+                    ", Color: " + color + ", Radius: " + radius;
+        }
+        // when fan is off it displays the color, radius, and lets the user know the fan is off
+        else{
+             return "Fan 2: " + "Color: " + color + ", Radius: " + radius + ", fan is off";
+            
+            
+            
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
